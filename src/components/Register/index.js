6191e03df04d81fb9958b2a72a5759e7cc1e87f8
Gary Hsiao <garyh6@users.noreply.github.com>
2020-02-26 23:02:40 -0800

    add register

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,39 +1,51 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import "./Login.scss";
+import "./Register.scss";
 
-const Login = () => {
+const Register = () => {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSubmit = async e => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:4000/api/user/login`, {
+      const res = await fetch(`http://localhost:4000/api/user/register`, {
         method: "POST",
         headers: new Headers({
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true
+          "Content-Type": "application/json"
         }),
         body: JSON.stringify({
           email,
-          password
-        }),
-        credentials: "include"
+          password,
+          firstName,
+          lastName
+        })
       });
       console.log("************ var ", res);
       history.push("/admin");
     } catch (err) {
-      console.log("************ login err", err);
+      console.log("************ Register err", err);
       // TODO fix after connecting to backend auth
     }
   };
 
   return (
     <form className="login-box" onSubmit={handleSubmit}>
-      <h1>Login</h1>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={e => setLastName(e.target.value)}
+      />
       <input
         type="text"
         placeholder="Email"
@@ -44,9 +56,9 @@ const Login = () => {
         placeholder="Password"
         onChange={e => setPassword(e.target.value)}
       />
-      <button type="submit">Login</button>
+      <button type="submit">Register</button>
     </form>
   );
 };
 
-export default Login;
+export default Register;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Admin from "../components/Admin"; // TODO make htis route private
 import Home from "../components/Home";
-
+import Register from "../components/Register";
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/register" exact component={Register} />
       <Route path="/admin" component={Admin} />
     </Switch>
   </BrowserRouter>
